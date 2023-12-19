2b35d60102b7d392d2876201e797071d7688d2d9
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2023-12-19 18:12:18 -0300

    feat: validateRepeatPassword completed

--- js/validators/validateRepeatPassword.js
+++ js/validators/validateRepeatPassword.js
@@ -1,11 +1,13 @@
 /**
- * 
+ *
  * @param {string} password Recibe una contraseña
  * @param {string} repeatPassword Recibe otra contraseña
- * @returns {bool} Devuelve un booleano dependiendo si las contraseñas de registro coinciden o no. 
+ * @returns {bool} Devuelve un booleano dependiendo si las contraseñas de registro coinciden o no.
  */
 
-
-export const validateRepeatPassword = (password, repeatPassword) =>{
-
+export const validateRepeatPassword = (password, repeatPassword) => {
+	if (password.localeCompare(repeatPassword) !== 0 || !password) {
+		return false
+	}
+	return true
 }
