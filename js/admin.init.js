95898a6fb3b9f2dad7ea7d9d0a035beb3084ec15
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2023-12-21 13:07:07 -0300

    feat: renderTableBody complete

--- js/admin.init.js
+++ js/admin.init.js
@@ -1,21 +1,24 @@
-import Navbar from "./components/Navbar.js";
-import TableRow from "./components/TableRow.js";
-import { getProducts } from "./services/getProducts.js";
-import { protectedAdminRoute } from "./routes/protectedAdmin.route.js";
+import Navbar from './components/Navbar.js'
+import TableRow from './components/TableRow.js'
+import { protectedAdminRoute } from './routes/protectedAdmin.route.js'
+import { getProducts } from './services/getProducts.js'
 
-document.addEventListener("DOMContentLoaded", () => {
-  Navbar();
-  protectedAdminRoute();
-  renderTableBody();
-});
+document.addEventListener('DOMContentLoaded', () => {
+	Navbar()
+	protectedAdminRoute()
+	renderTableBody()
+})
 
-const tableBody = document.getElementById("adminTableBody");
+const tableBody = document.getElementById('adminTableBody')
 
 /**
  * @returns {} Renderiza las filas de la tabla en el panel de admin.
  */
 
 export const renderTableBody = () => {
-  tableBody.innerHTML = "";
-  const products = getProducts();
-};
+	tableBody.innerHTML = ''
+	const products = getProducts()
+	products.forEach((product) => {
+		tableBody.innerHTML += TableRow(product)
+	})
+}
