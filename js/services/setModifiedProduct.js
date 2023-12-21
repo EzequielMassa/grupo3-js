8b286acec25e9202502e265800c0930c0cca60d0
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2023-12-21 18:18:57 -0300

    feat: setModifiedProducts complete

--- js/services/setModifiedProduct.js
+++ js/services/setModifiedProduct.js
@@ -1,10 +1,9 @@
 /**
- * 
+ *
  * @param {array} products Recibe un arreglo de productos
  * @returns Los setea en localStorage con la key "products"
  */
 
-
-export const setModifiedProducts = (products) =>{
-   
+export const setModifiedProducts = (products) => {
+	localStorage.setItem('products', JSON.stringify(products))
 }
