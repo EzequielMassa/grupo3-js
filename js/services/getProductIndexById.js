bea675c795d1262e07f2da2808d855edaffa7fbc
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2023-12-21 17:25:24 -0300

    feat: getProductIndexById complete

--- js/services/getProductIndexById.js
+++ js/services/getProductIndexById.js
@@ -1,12 +1,13 @@
-import { getProducts } from "./getProducts.js";
+import { getProducts } from './getProducts.js'
 
 /**
- * 
+ *
  * @param {string} id Recibe el Id de un producto
  * @returns Devuelve el indice en el arreglo de productos de localStorage del elemento que coincida con el id recibido por parametro.
  */
 
-export const getProductIndexById = (id) =>{
-    const products = getProducts()
-
+export const getProductIndexById = (id) => {
+	const products = getProducts()
+	const productIndex = products.findIndex((product) => product.id === id)
+	return productIndex
 }
