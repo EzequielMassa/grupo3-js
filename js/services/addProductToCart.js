da9b440077ac83e84eb11caee900f322231bb4f5
georginacostilla <georginacostilla.gc@gmail.com>
2023-12-26 20:36:44 -0300

    nuevos cambios en la rama

--- js/services/addProductToCart.js
+++ js/services/addProductToCart.js
@@ -9,8 +9,34 @@ import { setCartProducts } from "./setCartProducts.js";
  * 
  * @param {string} id Recibe el ID de un producto
  * @returns En el arreglo de productos del localStorage con la key correspondiente al id del usuario, agrega el producto, de ya existir dentro del carrito, aumentar su atributo "quantity" en 1 
- */
+*/
 
 export const addProductToCart = (id) => {
+    if (!getLoggedUser() || getLoggedUser().length === 0) {
+        notLoggedRoute();
+        return;
+    }
 
+    const userId = getLoggedUser().id;
+    const cartProducts = getCartProducts();
+    
+    console.log(cartProducts)
+
+
+    const productToAdd = getProductById(id);
+
+    const existingProductIndex = cartProducts.findIndex(
+        (product) => product.id === id
+    );
+
+    if (existingProductIndex !== -1) {
+        cartProducts[existingProductIndex].quantity += 1;
+    } else {
+        productToAdd.quantity = 1;
+        cartProducts.push(productToAdd);
+    }
+
+    setCartProducts(userId, cartProducts);
+    cartBadgeHandler();
+    return cartProducts;
 };
