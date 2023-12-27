64d2744fc53988c683b3079dcc9435ff801ec6cc
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2023-12-26 22:29:02 -0300

    feat: renderCartProducts complete

--- js/cart.init.js
+++ js/cart.init.js
@@ -1,43 +1,39 @@
-import { ProductCardCart } from "./components/CartCard.js"
-import { getCartProducts } from "./services/getCartProducts.js"
-import { discountPrice } from "./utils/discountPrice.js"
-import { formatCurrency } from "./utils/formatCurrency.js"
-
+import { ProductCardCart } from './components/CartCard.js'
+import { getCartProducts } from './services/getCartProducts.js'
 
 /**\
- * @returns Renderiza los productos y el precio total en el offcanvas del carrito 
+ * @returns Renderiza los productos y el precio total en el offcanvas del carrito
  */
-export const renderCartBody  = () =>{
-  class renderCartProducts {
-    
-  }
-  renderCartProducts()
-  renderCartTotalPrice()
+export const renderCartBody = () => {
+	renderCartProducts()
+	renderCartTotalPrice()
 }
 
-const totalPrice = document.getElementById("totalPrice")
+const totalPrice = document.getElementById('totalPrice')
 
 /**
- * @returns {} Renderiza la suma del precio de todos los productos en el carrito 
-*/
+ * @returns {} Renderiza la suma del precio de todos los productos en el carrito
+ */
 
-const renderCartTotalPrice = () =>{
- const cartProducts = getCartProducts()
+const renderCartTotalPrice = () => {
+	const cartProducts = getCartProducts()
 
-//  totalPrice.textContent = formatCurrency(...)
+	//  totalPrice.textContent = formatCurrency(...)
 }
 
 /**
  * @returns {} Renderiza los productos en el carrito
-*/
+ */
 
-const renderCartProducts = () =>{
-  const offcanvasBody = document.querySelector(".offcanvas-body")
-  const cartProducts = getCartProducts()
-  offcanvasBody.innerHTML = ""
-  if (ProductCardCart.length != 0){
-    offcanvasBody.innerHTML =+ "No tenes nada en el carrito"
-  }
-  //Utilizar componente ProductCardCart
-
+const renderCartProducts = () => {
+	const offcanvasBody = document.querySelector('.offcanvas-body')
+	const cartProducts = getCartProducts()
+	offcanvasBody.innerHTML = ''
+	if (cartProducts.length <= 0) {
+		offcanvasBody.innerHTML += 'No tenes nada en el carrito'
+		return
+	}
+	cartProducts.forEach(
+		(product) => (offcanvasBody.innerHTML += ProductCardCart(product))
+	)
 }
