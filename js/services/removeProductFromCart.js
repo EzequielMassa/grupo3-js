80868f6df0f79c8a1d693685bcf7fc5430a10409
georginacostilla <georginacostilla.gc@gmail.com>
2023-12-26 23:15:35 -0300

    removeProductFromCart complete

--- js/services/removeProductFromCart.js
+++ js/services/removeProductFromCart.js
@@ -4,19 +4,29 @@
  * @return En el arreglo de productos del carrito reduce en 1 la cantidad del que coincida con el id recibido, 
  * si la cantidad es 1, lo elimina del arreglo.
  */
+import { getCartProducts } from "./getCartProducts.js";
+import { setCartProducts } from "./setCartProducts.js";
+import {renderCartBody} from "../cart.init.js";
+import { getLoggedUser } from "./getLoggedUser.js";
 
 export const removeProductFromCart = (id) => {
+
+    const userId = getLoggedUser().id
     const cartProducts = getCartProducts();
-    const productIndex = cartProducts.findIndex((product) => product.id === id);
+    const productInCart = cartProducts.find((p) => p.id == id)
+    const productInCartQuantity = productInCart.quantity
 
-    if (productIndex !== -1) {
-        cartProducts[productIndex].quantity -= 1;
+    const productIndex = cartProducts.findIndex((p) => p.id == id);
 
-        if (cartProducts[productIndex].quantity === 0) {
-            cartProducts.splice(productIndex, 1);
-        }
-
-        setCartProducts(cartProducts);
+    if (productInCartQuantity == 1){
+        cartProducts.splice(productIndex,1)
+        setCartProducts(userId,cartProducts)
+        return;
     }
-    return cartProducts;
+
+    productInCart.quantity -- 
+    cartProducts[productIndex] = productInCart
+    setCartProducts(userId,cartProducts)
+
+    renderCartBody()
 };
